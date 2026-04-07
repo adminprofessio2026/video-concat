10d88250cf1f59d032473c0a26221b117dda5985
adminprofessio2026 <Admin@pgapp.info>
2026-04-07 16:50:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -56,21 +56,15 @@ async function processVideos(intro_url, main_url, outro_url, jobId) {
     console.log("main:", fs.existsSync(mainPath));
     console.log("outro:", fs.existsSync(outroPath));
 
-    const ffmpegCmd = [
-      "/usr/bin/ffmpeg",
-      "-y",
-      `-i "${introPath}"`,
-      `-i "${mainPath}"`,
-      `-i "${outroPath}"`,
-      '-filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[outv][outa]"',
-      '-map "[outv]"',
-      '-map "[outa]"',
-      "-c:v libx264",
-      "-c:a aac",
-      `"${outputPath}"`
-    ].join(" ");
+    const cmd = `/usr/bin/ffmpeg -y \
+-i "${introPath}" \
+-i "${mainPath}" \
+-i "${outroPath}" \
+-filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0][2:v:0][2:a:0]concat=n=3:v=1:a=1[outv][outa]" \
+-map "[outv]" -map "[outa]" \
+-c:v libx264 -c:a aac "${outputPath}"`;
 
-    exec(ffmpegCmd, (err, stdout, stderr) => {
+    exec(cmd, (err, stdout, stderr) => {
       console.log("FFmpeg stdout:", stdout);
       console.log("FFmpeg stderr:", stderr);
 
