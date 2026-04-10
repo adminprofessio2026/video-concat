5bf5745cf3b01a677b88ac790c991e16b0aaea67
adminprofessio2026 <Admin@pgapp.info>
2026-04-10 10:11:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -166,4 +166,8 @@ app.get("/download", (req, res) => {
 });
 
 // ✅ START SERVER
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
